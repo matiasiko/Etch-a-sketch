be0f900354fcfb0a2365e5b2831ebf1e42ab73bc
Matias Ikonen <alcatel1962@outlook.com>
2022-03-23 20:39:19 +0100

    h1 added

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-// Create container and wrapper
+
 const body = document.querySelector('body');
+// Create h1 element
+const h1 = document.createElement('h1')
+h1.textContent = 'Etc A Sketch';
+body.appendChild(h1);
+
+// Create container and wrapper
 const wrapper = document.createElement('div');
 const container = document.createElement('div');
 wrapper.classList.add('wrapper');
@@ -8,9 +14,7 @@ body.appendChild(wrapper);
 wrapper.appendChild(container);
 
 // Create H1 Element
-const h1 = document.createElement('h1')
-h1.textContent = 'Etc A Sketch';
-body.appendChild(h1);
+
 
 //Add square divs to container
 for (let i = 0; i < 64; i++) {
