c1cee2ff8563c04ccb2555739fc1bb7fbdd808bb
Matias Ikonen <alcatel1962@outlook.com>
2022-03-24 08:33:42 +0100

    Reset button and functionality added

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
-
 const body = document.querySelector('body');
 // Create h1 element
 const h1 = document.createElement('h1')
 h1.textContent = 'Etc A Sketch';
 body.appendChild(h1);
 
-// Create container and wrapper
+//Ceate Wrapper
 const wrapper = document.createElement('div');
+body.appendChild(wrapper);
+//Create Settings
+const settings = document.createElement('div');
+settings.classList.add('settings');
+wrapper.appendChild(settings);
+
+//Reset Button
+const button = document.createElement('button');
+button.classList.add('button');
+button.setAttribute('id', 'reset');
+button.textContent = "Clear"
+settings.appendChild(button);
+
+const reset = document.getElementById('reset');
+reset.addEventListener('click',(event) => {
+    const gridElements = document.getElementsByClassName('square');
+    for (let i = 0; i < gridElements.length; i++) {
+        gridElements[i].style.backgroundColor = "";
+
+    }
+})
+
+//Rainbow Button
+const button1 = document.createElement('button');
+button1.classList.add('button');
+button1.textContent = "Rainbow";
+settings.appendChild(button1);
+
+// Create container inside wrapper
 const container = document.createElement('div');
 wrapper.classList.add('wrapper');
 container.classList.add('container');
-body.appendChild(wrapper);
+
 wrapper.appendChild(container);
 
-// Create H1 Element
 
 
 //Add square divs to container
@@ -29,3 +56,4 @@ squares.forEach((square) => {
         event.target.style.backgroundColor = "#000";
     });
 });
+
